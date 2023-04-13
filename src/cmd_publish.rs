9f110ed68453f022c57d80d5c12872728dbca2b1
Bartłomiej Piotrowski <b@bpiotrowski.pl>
2023-04-13 11:20:50 +0200

    update tests

--- src/cmd_publish.rs
+++ src/cmd_publish.rs
@@ -437,6 +437,7 @@ mod tests {
         <value key="flathub::verification::timestamp">2023-01-01T00:00:00</value>
         <value key="flathub::verification::method">website</value>
         <value key="flathub::verification::website">example.com</value>
+        <value key="flathub::verification::login_is_organization">false</value>
     </custom>
 </component>
 </components>"#,
